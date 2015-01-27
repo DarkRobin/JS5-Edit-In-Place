90b76a7cff6eb7cffbba50774a68a5621b808ca6
DarkRobin <hulk@Hulk.local>
2015-01-26 22:58:41 -0700

    commit #2

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 $(document).on('ready', function() {
- 	$(".edit").on('click')
+  	$( '.edit' ).on('click', function() {
+  		console.log('edit');
+
+	});
+
+  	$(".comment").on("click", function () {
+  		var $edit = $(".edit");
+  		var $input = $(".input-field");
+  		var words = $edit.text().trim();
+
+  		$input.val(words);
+		$(".comment").hide();
+  		$(".input-container").css({"display" : "block" });
+  	});
+
+
+  	$('.submit-form').on('click', function () {
+		var $edit = $(".edit");
+  		var $input = $(".input-field");
+  		var words = $input.val();
+  			//$edit == $(".edit")
+  		$(".input-container").css({"display" : "none"});
+  		$edit.text(words);
+  		$(".comment").css({"display" : "block"});
+
+
+
+
+  	});
+
+
+
 });
